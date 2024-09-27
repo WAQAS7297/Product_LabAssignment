b33aa2dea016ad2fbc5eeda1edb3fd3b4f9cf6dc
Waqas Ramzan <waqasramzan88888@gmail.com>
2024-09-27 19:11:31 +0500

    New file name :products_stubs.py added by maintainer

---
+++ products_stubs.py
@@ -0,0 +1,13 @@
+# Maintainer: SP23_BAI_055
+# Collaborator: SP23_BAI_031
+class Product:
+    def __init__(self, name, amount, price):
+        self.name = name
+        self.amount = amount
+        self.price = price
+
+    def get_price(self, quantity):
+        pass
+
+    def make_purchase(self, quantity):
+        pass
